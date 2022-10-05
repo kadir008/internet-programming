818941cd43a247b7bc0c26305631a3135bb6e1df
kadir <86116178+kadir008@users.noreply.github.com>
2022-10-05 15:38:30 +0300

    Add files via upload

---
+++ 0310.php
@@ -0,0 +1,25 @@
+<?php
+
+    //yorum satırına dönüştürmek için.
+    #yorum satırına dönüştürmek için.
+	/*yorum satırına dönüştürmek için.*/
+	
+	/*$universite = "Dumlupınar";
+	$myo = "Universitesi";
+
+    echo $universite;
+	print "<br>";
+	echo $myo;*/
+
+	/*$universite = "Dumlupınar";
+	define("OKUL_ADI", "KTBMYO");
+	echo OKUL_ADI;*/
+    //mb : multi bayt.
+	//türkçe karakterler yok
+	//echo strlen("Karakter uzunluğu nedir?");
+	//echo str_word_count("Kelime sayiyor.");
+	//echo strrev("metni ters cevirir");
+	//echo strpos("Metnin kacinci karakterde oldugunu bulur.", "kacinci");
+	//echo str_replace("world", "Dolly", "Hello world!"); //1.aranan kelime , 2.değiştirilecek kelime, 3.çıkış.
+
+?>
